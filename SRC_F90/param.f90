80288f1af05e1c7e5f93fa302766b4c2a8172db8
JonC <claustre.j@gmail.com>
2016-09-23 09:07:00 -0400

    MEOP Cases with 0.3 Torr conditions and with power = 1, 5, 10, 20 W

--- SRC_F90/param.f90
+++ SRC_F90/param.f90
@@ -56,7 +56,7 @@ MODULE MOD_PARAM
 
 !  CHARACTER(*), PARAMETER :: DirFile = "./datFile/Santos/165W/"
 !  CHARACTER(*), PARAMETER :: DirFile = "./datFile/Default_RunDir/"
-  CHARACTER(*), PARAMETER :: DirFile = "./datFile/Rhanem_MEOP/0.3_Torr/"
+  CHARACTER(*), PARAMETER :: DirFile = "./datFile/Rhanem_MEOP/0.3_Torr/1W/"
 !  CHARACTER(*), PARAMETER :: DirFile = "./datFile/Belmonte/"
 !  CHARACTER(*), PARAMETER :: DirFile = "./datFile/post-Dischrg/760_Torr/Zoomed/0.001_microS/"
 !  CHARACTER(*), PARAMETER :: DirFile = "./datFile/Hamdan/5_torr/8_mm/"
